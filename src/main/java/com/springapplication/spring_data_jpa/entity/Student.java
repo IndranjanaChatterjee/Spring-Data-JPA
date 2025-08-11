4c1858a8fec2d07bb3871df0936ceaf697d1f884
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-11 19:06:15 +0530

    Initial commit

---
+++ src/main/java/com/springapplication/spring_data_jpa/entity/Student.java
@@ -0,0 +1,12 @@
+package com.springapplication.spring_data_jpa.entity;
+
+public class Student {
+
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
